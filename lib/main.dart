7aaa6d3547346e9ec7ec90f5da1e8cf9a5d88064
Oskar Zając <zoskar12@gmail.com>
2025-03-23 19:07:57 +0100

    setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:test_lab/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(TestLabApp());
 }
 
